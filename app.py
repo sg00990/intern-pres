eecb45a56f694812d876b046107ee2c052466ca0
Sarah Graddy <graddysa@icloud.com>
2024-05-29 12:51:11 -0400

    edit

--- app.py
+++ app.py
@@ -68,8 +68,6 @@ def main():
     choice = st.selectbox('label', options=["What is Streamlit?", "Streamlit Community Cloud", "My Apps", "Resources"], label_visibility="collapsed", index=None)
 
     col1, col2 = st.columns([2, .5])
-
-    col1.write('######')
     col1.title('Introduction to Streamlit')
     col2.write('######')
     col2.image('img/logo.png', width=100)
