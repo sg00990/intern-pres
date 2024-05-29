fcd6808bf6557e467fbbfc82f35bb104454327e6
Sarah Graddy <graddysa@icloud.com>
2024-05-29 12:46:16 -0400

    edit

--- app.py
+++ app.py
@@ -69,8 +69,9 @@ def main():
 
     col1, col2 = st.columns([2, .5])
 
+    col1.write('######')
     col1.title('Introduction to Streamlit')
-    col2.write('########')
+    col2.write('######')
     col2.image('img/logo.png', width=100)
 
 
